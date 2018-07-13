ddbf050ba5fae4590c470218b662e49d466596fe
koettpaj <carlkernell@gmail.com>
2018-07-13 19:19:35 +0200

    Bilder, halvfungerande röd fyrkant, lite skev zoom

--- canvas.js
+++ canvas.js
@@ -1,25 +1,62 @@
 const canvas = document.querySelector('canvas');
 const zoom = document.getElementById('zoom');
 
+const slider =document.getElementById('myRange');
+var zoomValue=1;
+canvas.addEventListener("wheel", zoomy);
+var xWidth=1280;
+var xMin=0;
+var yWidth=480;
+var yMin=0;
 
 
+function zoomy(event) {
+    if(event.deltaY<0){
+        if (zoomValue<4){
+        zoomValue+=0.2;
+        xMin=(event.layerX/zoomValue)-(1280/(zoomValue+0.2))/2;
+        yMin=(event.layerY/zoomValue)-(480/(zoomValue+0.2))/2;
+        xWidth=1280/(zoomValue+0.2);
+        yWidth=480/(zoomValue+0.2);
+        }}
+    if(event.deltaY>0){
+        if (zoomValue>1){
+        zoomValue-=0.2 }
+    }
+    drawImg()
+}
 
-canvas.addEventListener('click', function() {
+canvas.addEventListener('click', function(event) {
 
+    //console.log(event);
+   c.rect(event.layerX,event.layerY,150,100);
+   c.stroke();
+   //drawImg()
 
 }, false);
+canvas.addEventListener('mousemove', function(event) {
 
+    //alert(event.screenX)
+    //console.log(event);
+    drawImg()
+    //c.rect((event.layerX/zoomValue)-(1280/(zoomValue+0.2))/2,(event.layerY/zoomValue)-(480/(zoomValue+0.2))/2,1280/(zoomValue+0.2),480/(zoomValue+0.2));
+    c.rect((event.layerX)-(1280*0.4),event.layerY-(480*0.4),1280*0.8,480*0.8);
+    c.strokeStyle="#FF0000";
+    c.stroke();
+    //drawImg()
+
+}, false);
 const imgelement = document.querySelector('img');
 
 const c = canvas.getContext('2d');
 c.imageSmoothingEnabled = false;
-const img = new Video();
+const img = new Image();
 
 const reader = new FileReader();
 reader.onload = function (){
 
     img.onload=function(){
-        drawImg(0);
+        drawImg(1);
 
 
     };
@@ -33,6 +70,7 @@ function handleFileSelect(evt) {
     console.log("hello")
     console.log(evt)
     files = evt.target.files;// FileList object
+    slider.setAttribute("max", files.length.toString())
     console.log(files);
 
     loadImage(1)
--- controllers.js
+++ controllers.js
@@ -3,21 +3,25 @@ function sliderChange(value){
 }
 
 function loadImage(index){
-    reader.readAsDataURL(files[index-1])
+    console.log("LOAAADING");
+    reader.readAsDataURL(files[index-1]);
+
 }
 
-function drawImg(zoom){
-    zoom=parseInt(zoom);
+function drawImg(){
+
     console.log(img.width);
     canvas.width = img.width;
     canvas.height = img.height;
     c.imageSmoothingEnabled = false;
-    c.drawImage(img,0,0,1282,482);
-    //c.drawImage(img,500,300,15,15,0,0,1280,480);
+    c.imageSmoothingEnabled = false;
+    //c.scale(zoomValue, zoomValue)
+    //c.drawImage(img,0,0,1282,482);
+    c.drawImage(img,xMin,yMin,xWidth,yWidth,0,0,1280,480);
    //c.drawImage(img, img.width / zoom, img.height / zoom, img.width / zoom, img.height / zoom, 0, 0, canvas.width, canvas.height);
 
 }
 
-function zoomFunc(){
+function canvasDrag(){
     console.log("zoooming")
 }
