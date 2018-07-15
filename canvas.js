a2b15676a6db552f3f19cdd42514028e2c53815d
koettpaj <carlkernell@gmail.com>
2018-07-16 00:24:06 +0200

    Fungerande zoom, fungerande translate

--- canvas.js
+++ canvas.js
@@ -3,50 +3,195 @@ const zoom = document.getElementById('zoom');
 
 const slider =document.getElementById('myRange');
 var zoomValue=1;
-canvas.addEventListener("wheel", zoomy);
-var xWidth=1280;
-var xMin=0;
-var yWidth=480;
-var yMin=0;
+//canvas.addEventListener("wheel", wheelZoom);
+
+var leftMouseDown=false;
+var rightMouseDown=false;
+var latestMove=[null, null];
+var lastZoomPos=[null,null];
+var latestDrag=null;
+var startZoom=[null, null];
+let isBusy=false;
+
+function translate(xdiff,ydiff){
+    xdiff=xdiff/zoomValue;
+    ydiff=ydiff/zoomValue;
+    console.log("xdiff: "+typeof xdiff+" ydiff: "+ydiff);
+
+    let xMinTemp=img.zoomMinX;
+    let yMinTemp=img.zoomMinY;
+    let widthXTemp=img.zoomWidthX;
+    let widthYTemp=img.zoomWidthY;
+    xMinTemp=xMinTemp-xdiff;
+    yMinTemp=yMinTemp-ydiff;
 
 
-function zoomy(event) {
-    if(event.deltaY<0){
-        if (zoomValue<4){
-        zoomValue+=0.2;
-        xMin=(event.layerX/zoomValue)-(1280/(zoomValue+0.2))/2;
-        yMin=(event.layerY/zoomValue)-(480/(zoomValue+0.2))/2;
-        xWidth=1280/(zoomValue+0.2);
-        yWidth=480/(zoomValue+0.2);
-        }}
-    if(event.deltaY>0){
-        if (zoomValue>1){
-        zoomValue-=0.2 }
+    if(xMinTemp<0){
+        xMinTemp=0;
     }
+    if(yMinTemp<0){
+        yMinTemp=0;
+    }
+    if(xMinTemp+widthXTemp>1280){
+        xMinTemp=1280-widthXTemp;
+    }
+    if(yMinTemp+widthYTemp>480){
+        yMinTemp=480-widthYTemp;
+    }
+
+    img.zoomMinX= xMinTemp;
+    img.zoomMinY= yMinTemp;
     drawImg()
+
+
+
 }
 
-canvas.addEventListener('click', function(event) {
+function dragZoom(diff){
+    //console.log(diff);
+    let xMin=0;
+    let yMin=0;
+    let xWidth=1280;
+    let yWidth=480;
+    if(diff<0 &&zoomValue<8){
+        x=startZoom[0];
+        y=startZoom[1];
+        zoomValue+=Math.abs(diff/100*zoomValue);
+        if(zoomValue>8){
+            zoomValue=8;
+        }
+        xMin=Math.round(((x)-(1280/(zoomValue*2))));
+        yMin=Math.round(y-(480/(zoomValue*2)));
+        xWidth=Math.round(1280/(zoomValue));
+        yWidth=Math.round(480/(zoomValue));}
+    else if(diff>0 &&zoomValue>1){
+        x=startZoom[0];
+        y=startZoom[1];
+        zoomValue-=Math.abs(diff/100*zoomValue);
+        if(zoomValue<1){
+            zoomValue=1;
+        }
+        xMin=Math.round(((x)-(1280/(zoomValue*2))));
+        yMin=Math.round(y-(480/(zoomValue*2)));
+        xWidth=Math.round(1280/(zoomValue));
+        yWidth=Math.round(480/(zoomValue));
+    } else{
+        return
+    }
 
+    if(xMin<0){
+        xMin=0;
+    }
+    if(yMin<0){
+        yMin=0;
+    }
+    if(xMin+xWidth>1280){
+        xMin=1280-xWidth;
+    }
+    if(yMin+yWidth>480){
+        yMin=480-yWidth;
+    }
+
+    img.zoomMinX= xMin;
+    img.zoomMinY= yMin;
+    img.zoomWidthX = xWidth;
+    img.zoomWidthY = yWidth;
+
+    //setTimeout(wtf,10000);
+    drawImg();
+    console.log("done");
+
+
+
+
+
+}
+
+function wtf(){
+    alert("balls")
+}
+
+function mapRange(xTrue,yTrue){
+    var relX=Math.round((parseInt(xTrue)/1280)*(img.zoomWidthX)+img.zoomMinX);
+    var relY=Math.round((parseInt(yTrue)/480)*(img.zoomWidthY)+img.zoomMinY);
+    console.log(relX + "and "+relY);
+    return [relX, relY];
+
+
+}
+canvas.addEventListener('mousedown', function(event) {
+
+    if (event.button.toString()==="0"){
+        leftMouseDown=true;
+    }
+    if (event.button.toString()==="2"){
+        rightMouseDown=true;
+    }
+    event.preventDefault()
     //console.log(event);
-   c.rect(event.layerX,event.layerY,150,100);
-   c.stroke();
+    //c.rect(event.layerX,event.layerY,150,100);
+    //c.stroke();
+    //drawImg()
+
+});
+canvas.addEventListener('contextmenu', event => event.preventDefault());
+canvas.addEventListener('mouseup', function(event) {
+    leftMouseDown=false;
+    rightMouseDown=false;
+    startZoom=[null, null];
+    latestDrag=null;
+    latestMove=[null, null];
+    event.preventDefault()
+});
+
+canvas.addEventListener('mouseleave', function(event) {
+    leftMouseDown=false;
+    rightMouseDown=false;
+    latestMove=[null, null];
+    startZoom=[null, null];
+    latestDrag=null;
+});
+
+
+
+
+
+canvas.addEventListener('click', function(event) {
+    //mapRange(event.layerX,event.layerY)
+    //console.log(event);
+   //c.rect(event.layerX,event.layerY,150,100);
+   //c.stroke();
    //drawImg()
 
 }, false);
 canvas.addEventListener('mousemove', function(event) {
 
-    //alert(event.screenX)
-    //console.log(event);
-    drawImg()
-    //c.rect((event.layerX/zoomValue)-(1280/(zoomValue+0.2))/2,(event.layerY/zoomValue)-(480/(zoomValue+0.2))/2,1280/(zoomValue+0.2),480/(zoomValue+0.2));
-    c.rect((event.layerX)-(1280*0.4),event.layerY-(480*0.4),1280*0.8,480*0.8);
-    c.strokeStyle="#FF0000";
-    c.stroke();
-    //drawImg()
+    if(leftMouseDown){
 
-}, false);
-const imgelement = document.querySelector('img');
+        isBusy=true;
+        if(startZoom[0]==null){
+            posses=mapRange(event.layerX, event.layerY);
+            startZoom=[posses[0],posses[1]];
+
+        }
+        if(latestDrag!=null){
+            dragZoom(latestDrag-event.layerX);
+        }
+
+        latestDrag=event.layerX;
+    }
+
+    if(rightMouseDown){
+        if(latestMove[0]!=null){
+            translate(event.layerX-latestMove[0],event.layerY-latestMove[1])
+        }
+
+        latestMove=[event.layerX,event.layerY];
+    }
+
+
+});
+
 
 const c = canvas.getContext('2d');
 c.imageSmoothingEnabled = false;
@@ -56,11 +201,16 @@ const reader = new FileReader();
 reader.onload = function (){
 
     img.onload=function(){
-        drawImg(1);
+        drawImg();
 
 
     };
     img.src = reader.result;
+    if (img.zoomMinX==null){
+    img.zoomMinX=0;
+    img.zoomMinY=0;
+    img.zoomWidthX=1280;
+    img.zoomWidthY=480;}
     //imgelement.src=img
 
    // c.fillRect(20,20,150,100)
--- controllers.js
+++ controllers.js
@@ -10,17 +10,19 @@ function loadImage(index){
 
 function drawImg(){
 
-    console.log(img.width);
     canvas.width = img.width;
     canvas.height = img.height;
     c.imageSmoothingEnabled = false;
     c.imageSmoothingEnabled = false;
     //c.scale(zoomValue, zoomValue)
     //c.drawImage(img,0,0,1282,482);
-    c.drawImage(img,xMin,yMin,xWidth,yWidth,0,0,1280,480);
+
+    console.log("image dimensions: "+img.zoomMinX+" "+img.zoomMinY+" "+img.zoomWidthX+" "+img.zoomWidthY+" zv: "+zoomValue);
+    c.drawImage(img,img.zoomMinX,img.zoomMinY,img.zoomWidthX,img.zoomWidthY,0,0,1280,480);
    //c.drawImage(img, img.width / zoom, img.height / zoom, img.width / zoom, img.height / zoom, 0, 0, canvas.width, canvas.height);
 
 }
+
 
 function canvasDrag(){
     console.log("zoooming")
